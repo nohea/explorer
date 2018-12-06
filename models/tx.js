6def79ec9dbac2745dc70dfc55cf39c851b58637
Raul Nohea Goodness <raulg@hekili.net>
2018-12-06 08:31:31 -1000

    add block.flags to display and mongodb so we can easily see proof-of-work, proof-of-stake or stake-modifier

--- models/tx.js
+++ models/tx.js
@@ -9,6 +9,7 @@ var TxSchema = new Schema({
   timestamp: { type: Number, default: 0 },
   blockhash: { type: String },
   blockindex: {type: Number, default: 0},
+  flags: { type: String, lowercase: true },
 }, {id: false});
 
 module.exports = mongoose.model('Tx', TxSchema);
